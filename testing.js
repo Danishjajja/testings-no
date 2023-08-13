3d22bd8d2eecd58ea82764c86fcc6a8639434e2d
Danish jajja <danishjajja86@gmail.com>
2023-08-13 12:12:14 +0500

    testing

---
+++ testing.js
@@ -0,0 +1,7 @@
+"use strict";
+// var studentNames:string[]=["ali","ahmad","danish","jajja","hamid"]
+// studentNames.splice(3,10,"jutt")
+// console.log(studentNames);
+let todos = ["learn arry", "learn arry methods"];
+todos.splice(1, 0, "how are you");
+console.log(todos);
---
+++ testing.ts
@@ -0,0 +1,13 @@
+// var studentNames:string[]=["ali","ahmad","danish","jajja","hamid"]
+// studentNames.splice(3,10,"jutt")
+// console.log(studentNames);
+let todos:string[]=["learn arry","learn arry methods"];
+var updatetodos=todos.splice(1,0,"how are you")
+console.log(todos);
+
+
+
+
+
+
+
