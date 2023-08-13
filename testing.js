35c6de05a220841ffb7b76211bdee1f3d85c3e87
Danish jajja <danishjajja86@gmail.com>
2023-08-13 18:58:13 +0500

    table of 5

--- testing.js
+++ testing.js
@@ -6,11 +6,18 @@
 // var updatetodos=todos.splice(1,0,"how are you")
 // console.log(todos);
 // write a program having arrays of todos if todos are in odd delete one from center of array otherwise remove two todos from center
-var todoss = ["ali", "ahmad", "danish", "jajja", "hamid", "ham"];
-if (todoss.length % 2 == 0) {
-    todoss.splice(todoss.length / 2, 2);
+// var todoss:string[]=["ali","ahmad","danish","jajja","hamid","ham" ]
+// if(todoss.length%2==0){
+// todoss.splice(todoss.length/2,2)
+// }
+// else{
+//     todoss.splice(todoss.length/2,1)
+//     }
+//     console.log(todoss);
+var num1 = 1;
+var num2 = 2;
+while (num1 <= 10) {
+    var num3 = num2 * num1;
+    console.log("2*" + num1 + "=", num3);
+    num1 = num1 + 1;
 }
-else {
-    todoss.splice(todoss.length / 2, 1);
-}
-console.log(todoss);
--- testing.ts
+++ testing.ts
@@ -7,14 +7,24 @@
 
 
 // write a program having arrays of todos if todos are in odd delete one from center of array otherwise remove two todos from center
-var todoss:string[]=["ali","ahmad","danish","jajja","hamid","ham" ]
-if(todoss.length%2==0){
-todoss.splice(todoss.length/2,2)
-}
-else{
-    todoss.splice(todoss.length/2,1)
-    }
-    console.log(todoss);
+// var todoss:string[]=["ali","ahmad","danish","jajja","hamid","ham" ]
+// if(todoss.length%2==0){
+// todoss.splice(todoss.length/2,2)
+// }
+// else{
+//     todoss.splice(todoss.length/2,1)
+//     }
+//     console.log(todoss);
+
+    var num1:number = 1
+    var num2:number =2
+    while(num1<=10){
+        var num3:number = num2*num1
+        console.log("2*"+num1+"=",num3);
+        num1=num1+1
+        }
+
+
     
 
 
