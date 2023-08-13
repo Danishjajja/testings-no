6ab4e152575b125e6281f0926c1824ada517fcb6
Danish jajja <danishjajja86@gmail.com>
2023-08-13 12:19:08 +0500

    testing

--- testing.ts
+++ testing.ts
@@ -1,7 +1,7 @@
 // var studentNames:string[]=["ali","ahmad","danish","jajja","hamid"]
 // studentNames.splice(3,10,"jutt")
 // console.log(studentNames);
-let todos:string[]=["learn arry","learn arry methods"];
+let todos:string[]=["learn ary","learn aray methods"];
 var updatetodos=todos.splice(1,0,"how are you")
 console.log(todos);
 
